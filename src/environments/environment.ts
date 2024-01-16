068c39769a3f5d2047321a02b82551605eddaf6d
stefano_2007 <stefano_2007@hotmail.com>
2024-01-15 23:24:11 -0300

    Renovando token API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url_API: 'https://api.github.com',
-  github_Token: 'WjJsMGFIVmlYM0JoZEY4eE1VRk1SbE5MTlZFd1oyWTRVRWM0ZDJnMVRtRXpYM0IyT0RabFpIWnZhWEV3Y1VsUGRUSktaWEl4T0VJM1pFRkNkVzk1ZEdWTk5sZHZXa2Q0UmxaVE0wVkdUa1ZVUzFkVFVFRnliR1ZVZUZacQ=='
+  github_Token: 'WjJsMGFIVmlYM0JoZEY4eE1VRk1SbE5MTlZFd05sbzJPRWRHU2tWUmQzTlJYM05qUTFaMGFuRllSVkZuTm1SUlZYWmhjVFJsUmsxS01HNVpjbW8wWVVzNWJrWlpRMmxNU25sbk5sQlBOa3RGTkVSRldrOXFWVk5QUmt4Sg=='
 };
