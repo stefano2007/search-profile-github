1c1258aee88b7f1a2600aede27a850576a93f0d3
Stefano Silva <stefano_2007@hotmail.com>
2024-04-17 23:02:13 -0300

    Atualizando token api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   url_API: 'https://api.github.com',
-  github_Token: 'WjJsMGFIVmlYM0JoZEY4eE1VRk1SbE5MTlZFd05sbzJPRWRHU2tWUmQzTlJYM05qUTFaMGFuRllSVkZuTm1SUlZYWmhjVFJsUmsxS01HNVpjbW8wWVVzNWJrWlpRMmxNU25sbk5sQlBOa3RGTkVSRldrOXFWVk5QUmt4Sg=='
+  github_Token: 'WjJsMGFIVmlYM0JoZEY4eE1VRk1SbE5MTlZFd04zSlhUMjF5WTFOSE5tSnNYMEpRUnpkaGJrOU9RbkJGTlZGWU5XSnpVMjlYUkdsR1VtRm9OV1pPY2pGVmNXRTFTRFUxVUdwU1IzVlFOMXBPUWtOWE1uZGpjVkUzWTFSUA=='
 };
